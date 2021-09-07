855e75741387c1237f5d6b1b04a13a54d1cd216a
LavanyaMalan <60563692+LavanyaMalan@users.noreply.github.com>
2021-09-07 15:04:57 +0530

    Add files via upload

---
+++ LoginSalesForce.java
@@ -0,0 +1,43 @@
+package assessmentweek2;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginSalesForce {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://login.salesforce.com");
+		
+		driver.findElementById("username").sendKeys("earth@testleaf.com");
+		driver.findElementById("password").sendKeys("Bootcamp$123");
+		driver.findElementById("Login").click();
+		
+		Thread.sleep(10000);
+	
+		driver.findElementByXPath("//div[@class = 'appLauncher slds-context-bar__icon-action']//button").click();
+	
+		Thread.sleep(5000);
+		
+		driver.findElementByXPath("//button[text() = 'View All']").click();
+		Thread.sleep(5000);
+		driver.findElementByXPath("//label[text() = 'Search apps or items...']/following::input").sendKeys("contracts");
+		
+		driver.findElementByXPath("//mark[text() = 'Contracts']").click();
+		
+		driver.findElementByXPath("//span[text() = 'Recently Viewed | Contracts' ]/following::lightning-primitive-icon[1]");
+		
+		driver.findElementByXPath("//span[text() = 'New Contract']").click();
+		
+		
+		//driver.findElementByXPath("//p[text() = 'Contracts']").click();
+		
+	
+	
+	}
+
+}
